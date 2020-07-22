880d76243967ba24b0492f009211860689806637
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-07-22 15:12:55 -0400

    understand the problem

--- Section_4_Problem_Solving.js
+++ Section_4_Problem_Solving.js
@@ -1,6 +1,22 @@
 // Problem Solving
 
 // 1. Understand the problem
+// - Can I restate the problem in my own words?
+// - What are the inputs that go into the problem?
+// - What are the expected solution outputs?
+// - Can the outputs be determined from the inputs? (sometimes this isn't easy to see)
+// - How should I label the important pieces of data that are part of a problem?
+
+// Write a function which takes two numbers and returns their sum
+
+function sum(x, y){
+    return x + y;
+} 
+
+
+
+
+
 // 2. Explore concrete examples
 // 3. Break it down
 // 4. Solve / simplify
