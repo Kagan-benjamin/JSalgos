204cc9c0fa222985415a7eab1170d3373d75e79d
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-07-27 16:52:26 -0400

    averagePair and isSubsequence, mult pointers exercises

--- Section_6_Exercises.js
+++ Section_6_Exercises.js
@@ -58,3 +58,35 @@ function sameFrequency(a, b){
     }
     return false;
   }
+
+
+
+
+  function averagePair(arr, n){                        // multiple pointers
+
+    let start = 0;
+    let end = arr.length - 1;
+    while(start < end){
+        let avg = (arr[start] + arr[end])/2
+        if (avg === n){
+            return true;
+        } else if(avg < n){
+            start++
+        } else {
+            end--
+        }
+    }
+    return false;
+}
+
+function isSubsequence(str1, str2) {                   // multiple pointers
+    var i = 0;
+    var j = 0;
+    if (!str1) return true;
+    while (j < str2.length) {
+      if (str2[j] === str1[i]) i++;
+      if (i === str1.length) return true;
+      j++;
+    }
+    return false;
+  }
