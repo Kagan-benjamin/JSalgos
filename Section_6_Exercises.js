e280d0ccf24e9d49195ad93048f93a46b8499fee
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-07-27 16:28:27 -0400

    sameFrequency, areThereDuplicates

--- Section_6_Exercises.js
+++ Section_6_Exercises.js
@@ -1,3 +1,6 @@
+// Use frequency counter technique to check whether 2 incoming numbers have equal
+// frequencies of each digit. 
+
 function sameFrequency(a, b){
     let x = a.toString()
     let y = b.toString()
@@ -23,4 +26,35 @@ function sameFrequency(a, b){
         }
     }
     return true;
+  }
+
+// Implement a function called areThereDuplicates which accepts a variable number of
+// arguments and checks whether there are any duplicates among the arguments passed
+// in. You can solve this using the frequency counter or mult pointers pattern.
+
+
+  function areThereDuplicates(...args) {            // multiple pointers
+    args.sort((a,b) => a > b);
+    let idxStart = 0;
+    let idxEnd = 1;
+    
+    while(idxEnd < args.length){
+        if(args[idxStart] === args[idxEnd]){
+            return true;
+        } 
+        idxStart++;
+        idxEnd++;
+    }
+    return false;
+  }
+
+  function areThereDuplicates() {                   // frequency counter
+    let collection = {}
+    for(let val in arguments){
+      collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+    }
+    for(let key in collection){
+      if(collection[key] > 1) return true
+    }
+    return false;
   }
