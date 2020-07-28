1fa00b9382bd826526279a988e12ce943dad04af
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-07-28 17:30:15 -0400

    binary search implementation

--- Section_10_Searching_Algos.js
+++ Section_10_Searching_Algos.js
@@ -12,7 +12,7 @@
 // current element is equal to the value. 3. If it is, return the index at which
 // the element is found.
 
-function linearSearch(arr, n){
+function linearSearch(arr, n){                    // O(n)
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === n){
             return i;
@@ -22,7 +22,34 @@ function linearSearch(arr, n){
 }
 
 
+// - Implement binary search on sorted arrays. //
+// .Binary search is significantly faster than linear search
+// .Rather than eliminating one element at a time, you can eliminate half 
+// .*** Binary search only works on SORTED arrays //
 
-// - Implement binary search on sorted arrays.
+// **write a binary search function that:
+// 1. Accepts a sorted array and a value.  2. Create two variables, left and right 
+// pointers for beginning and end of the array. 3. Loop while the left pointer
+// is smaller than the right pointer. 4. during loop, create a middle pointer.  
+// 5. check value at mid, if too small, move left pointer up
+// 6. check value at mid, if too large, move right pointer down
+// 7. if value matches, return the index.  8. If no match, return -1.
+
+function myBinarySearch(arr, n){
+    let start = 0;
+    let end = arr.length - 1;
+    let mid = Math.floor((start + end) / 2);
+    while(arr[mid] !== n && start <= end){
+        if(arr[mid] < n){
+            start = mid + 1;
+        } else  {
+            end = mid - 1;
+        }
+        mid = Math.floor((start + end) /2)
+    }
+    if(arr[mid] === n) return mid;
+    return -1;
+}
+
 // - Implement a naive string search algorithm.
 // - Implement the KMP string searching algorithm.
--- Section_8 and 9_Recursion_Exercises.js
+++ Section_8 and 9_Recursion_Exercises.js
@@ -108,3 +108,6 @@ function capitalizeFirst(arr){
     return ans;
 }
 
+// Write a function called nestedEvenSum. Return the sum of all even numbers 
+// in an object which may contain nested objects.
+
