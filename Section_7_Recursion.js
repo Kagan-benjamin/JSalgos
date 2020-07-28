ff49a9547c97f62b8e848675fad312f4db7a9b94
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-07-28 13:09:41 -0400

    intro to recursion

---
+++ Section_7_Recursion.js
@@ -0,0 +1,44 @@
+                             // Recursion //
+
+// | Define Recursion and how it can be used -
+
+// .A process (function) that calls itself
+// .examples: JSON.parse/.stringify, getElementById/querySelector, DOM traversal algos
+// .Object traversal, traversal of complex data structures
+// .Sometimes a cleaner alternative to iterative solutions  
+
+
+// | Visualize the call stack to better debug and understand recursive functions -
+
+// .Stack data structure - any time a function is invoked it is placed(pushed)
+// .onto the top of the call stack.
+// .When JS sees the return keyword or when the function ends, the compiler will
+// .remove(pop) the action from the top of the stack.
+// **Recursive functions keep pushing new functions (itself) onto the call stack.
+
+// .Invoke the SAME function with DIFFERENT input until you reach your base case
+// .Base Case: the condition when the recursion ends.
+// ** 1. Base Case  // 2. Different Input **
+
+function countDown(n){                  // Recursive
+    if(n <= 0){
+        console.log('done!');
+        return;
+    }
+    console.log(n);
+    n--;
+    countDown(n);
+}
+
+function iterativeCountDown(n){         // Iterative
+    for(let i = n; i > 0; i--){
+        console.log(i);
+    }
+    console.log('done!');
+}
+
+
+
+// | Use helper method recursion and pure recursion to solve more difficult problems -
+
+
