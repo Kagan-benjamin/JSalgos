1e9dc2f3173775521353fa2ce383efdcbaa6eb65
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-07-29 15:42:41 -0400

    Bubble, Selection, Insertion Sort comparison

---
+++ Section_14_BubSelInsCompare.js
@@ -0,0 +1,15 @@
+            // Big O of Elementary/Quadratic Sorting Algorithms //
+
+// Algorithm: | Time (best): | Time (avg): | Time (worst): | Space complexity:
+// Bubble Sort |    O(n)     |    O(n^2)   |     O(n^2)    |      O(1)
+// Insertion Sort | O(n)     |    O(n^2)   |     O(n^2)    |      O(1)
+// Selection Sort | O(n^2)   |    O(n^2)   |     O(n^2)    |      O(1)
+
+// - Nearly sorted data: bubble sort + insertion short perform well. Selection suffers
+// - Space complexity is typically constant. 
+// - Maintenance of running sort: use Insertion Sort!
+
+// .All 3 of these algorithms are roughly equivalent in the grand scheme, excepting 
+// special cases. All have average time complexities that are quadratic. Excel with 
+// small data sets. We can do better, but we need more complex algorithms.
+
