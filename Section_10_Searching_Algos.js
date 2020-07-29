3ccac464ceb6fce422239783f6cb44769d25d104
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-07-29 12:16:06 -0400

    naive string search

--- Section_10_Searching_Algos.js
+++ Section_10_Searching_Algos.js
@@ -35,12 +35,12 @@ function linearSearch(arr, n){                    // O(n)
 // 6. check value at mid, if too large, move right pointer down
 // 7. if value matches, return the index.  8. If no match, return -1.
 
-function myBinarySearch(arr, n){
+function myBinarySearch(arr, n){               // Worst/Avg Case: O(logn)
     let start = 0;
     let end = arr.length - 1;
-    let mid = Math.floor((start + end) / 2);
-    while(arr[mid] !== n && start <= end){
-        if(arr[mid] < n){
+    let mid = Math.floor((start + end) / 2);   // 16 element | 4 steps | log2(16) = 4
+    while(arr[mid] !== n && start <= end){     // 32 element | 5 steps | log2(32) = 5
+        if(arr[mid] < n){                                     // log2(n)
             start = mid + 1;
         } else  {
             end = mid - 1;
@@ -51,5 +51,34 @@ function myBinarySearch(arr, n){
     return -1;
 }
 
-// - Implement a naive string search algorithm.
+// - Implement a naive string search algorithm. //
+//. Suppose you want to count the number of times that a smaller string appears
+//. in a longer string. A straightforward approach involves checking pairs of
+//. characters individually.
+
+//. 1. Loop over the longer string.  2. Loop over the shorter string
+//. 3. If the characters don't match, break out of the inner loop.
+//. 4. If the characters do match, keep going.
+//. 5. If you complete the inner loop and find a match, increment
+//. the count of matches.   6. Return the count.
+
+function myNaiveStringSearch(str1, str2){
+    let count = 0;
+    for(let i = 0; i < str1.length; i++){
+        for(let j = 0; j < str2.length; j++){
+            if(str2[j] !== str1[i + j]){
+                break;
+            }
+            if(j === str2.length - 1){
+                count++;
+            }
+
+        }
+    }
+    return count;
+}
+
+
+
+
 // - Implement the KMP string searching algorithm.
