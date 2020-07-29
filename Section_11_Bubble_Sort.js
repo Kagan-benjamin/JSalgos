84f7e92d02f492cabbe9d44c9042051362d553c7
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-07-29 13:52:46 -0400

    bubble sort intro

---
+++ Section_11_Bubble_Sort.js
@@ -0,0 +1,17 @@
+  // What is sorting? //
+
+// .Sorting is the process of rearranging the items in a collection (e.g. an array)
+// .so that the items are in some kind of order.
+// .Ex: Sorting numbers by size. Sorting names alphabetically. Movies based on release year
+
+  //  Why?   //
+
+// .Sorting is an incredibly common task, so it's good to know how it works.
+// .There are many different ways to sort things, and different techniques have their
+// .own advantages and disadvantages.
+// **Sorting Algorithm Animations website**
+
+  // Objectives //
+  
+// .Implement bubble sort | implement selection sort | implement insertion sort
+// .Understand why it is important to learn these simpler sorting algorithms.
