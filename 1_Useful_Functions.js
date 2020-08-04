8ec88c955478c1541913fb8f13521384973a164c
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-08-04 11:41:23 -0400

    Useful Functions/Examples

---
+++ 1_Useful_Functions.js
@@ -0,0 +1,50 @@
+function stringifyNumbers(obj){
+    let newObj = {};
+    for(let key in obj){
+      if(typeof obj[key] === 'number'){
+        newObj[key] = obj[key].toString();
+      } else if(typeof obj[key] === 'object' && !Array.isArray(obj[key])){
+        newObj[key] = stringifyNumbers(obj[key]);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+}
+
+// Pure Recursion
+
+function collectStrings(obj) {
+    var stringsArr = [];
+    for(var key in obj) {
+        if(typeof obj[key] === 'string') {
+            stringsArr.push(obj[key]);
+        }
+        else if(typeof obj[key] === 'object') {
+            stringsArr = stringsArr.concat(collectStrings(obj[key]));
+        }
+    }
+ 
+    return stringsArr;
+}
+
+// Helper Recursion
+
+function collectStrings(obj) {
+    var stringsArr = [];
+ 
+    function gatherStrings(o) {
+        for(var key in o) {
+            if(typeof o[key] === 'string') {
+                stringsArr.push(o[key]);
+            }
+            else if(typeof o[key] === 'object') {
+                return gatherStrings(o[key]);
+            }
+        }
+    }
+ 
+    gatherStrings(obj);
+ 
+    return stringsArr;
+}
--- Section_18_Data_Structures_Intro.js
+++ Section_18_Data_Structures_Intro.js
@@ -59,15 +59,15 @@ let student1 = new Student('these', 'notes')
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-class DataStructure(){
-    constructor(){
-        // what default properties should it have?
-    }
+//      class DataStructure(){
+//            constructor(){
+                // what default properties should it have?
+//            }
 
-    someInstanceMethod(){
-        // what should each object created from this class be able to do?
-    }
-}
+//            someInstanceMethod(){
+                // what should each object created from this class be able to do?
+//            }
+//        }
 
 
 
