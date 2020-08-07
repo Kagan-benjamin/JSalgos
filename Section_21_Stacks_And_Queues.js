434919457d3f65628c5aef7bfa224ede0f6bb357
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-08-07 11:47:15 -0400

    Stack with linked list and Big-O

--- Section_21_Stacks_And_Queues.js
+++ Section_21_Stacks_And_Queues.js
@@ -7,9 +7,10 @@
 //              (Tail)              (Head)
 
 // Use cases for stacks
-// .manage function invocations
-// .undo/redo 
+// .manage function invocations (Call Stack)
+// .undo/redo operations
 // .history objects (routing) are treated like a stack
+// .NOT A BUILT-IN DATA STRUCTURE IN JAVASCRIPT, but easy to implement.
 // .used to store history of data in tree/graph comparison
 // .more than one way to implement stacks
 
@@ -26,5 +27,51 @@ stack.pop() // [site 2] => [site 1]
 stack.pop() // [site 1] => []
 
 // Linked List Stack
+
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    push(val){
+        const newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.length;
+    }
+
+    pop(){
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.length === 1){
+            this.first = null;
+            this.last = null;
+        } else {
+            this.first = this.first.next;
+        }
+        this.length--
+        return temp.val
+    }
+}
+
+// Big-O of Stacks:
+
+// Insertion - O(1) | Removal - O(1)  No need to iterate
+// Search - O(n) | Access - O(n)  Necessary to iterate through linked list
 
 
