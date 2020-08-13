393e3c452509fc6e13d6af91cb3c5d3810f8148c
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-08-13 11:49:12 -0400

    Binary Search Tree intro and use cases

---
+++ Section_22_Binary_Search_Trees.js
@@ -0,0 +1,20 @@
+                            // Binary Search Trees //
+
+// What is a tree? 
+
+// .A data structure that consists of nodes in a parent/child relationship  
+//** .Lists are linear whereas trees are non-linear
+// .Tree nodes CANNOT reference sibling nodes, only parent/child nodes.
+// .Each tree needs 1 entry Root Node. The 'root' is the top node in a tree.
+// .A child node is directly connected to another node when moving away from the root.
+// .A parent node is the converse concept relative to child nodes.
+// .Sibling nodes have the same parent.
+// .Leaf - a node with no children.  Edge - The connection between nodes (arrow/ling)
+
+// Use cases for trees:
+// .HTML Document-Object-Model (DOM) - nested html data structure
+// .Network Routing
+// .Abstract Syntax Tree
+// .Artificial Intelligence (decision trees)
+// .Folders within operating systems (folder trees)
+// .JSON objects  
