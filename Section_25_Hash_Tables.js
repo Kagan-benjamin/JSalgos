e35bf18e2e3f4a0ea4a02cfa42fe213c209e334c
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-08-26 11:39:02 -0400

    Basic Hash implementation

--- Section_25_Hash_Tables.js
+++ Section_25_Hash_Tables.js
@@ -23,7 +23,31 @@ readability?
 
 
     .What is a hashing algorithm?
-    .What makes a good hashing algorithm?
     .Understand how collisions occur in a hash table.
     .Handle collisions using separate chaining or linear probing.
-*/                                    
+*/                              
+
+/* Hash Functions:
+.Takes in data of arbitrary size and returns data of fixed size. 1-way
+.What makes a good hashing algorithm?
+    -Fast (constant time)
+    -Doesn't cluster outputs at specific indices, but distributes uniformly
+    -Deterministic (same input yields same output, always)
+*/
+
+// Basic (Bad) Hash Function Example:
+
+'a'.charCodeAt(0) //=> 97
+'hi'.charCodeAt(0) //=> 104
+hash('pink', 10)
+
+function hash(key, arrLength){
+    let total = 0;
+    for(let i = 0; i < str.length; i++){
+        total += (str.charCodeAt(i) % arrLength);
+    }
+    return total;
+}
+// .Only hashes strings.
+// .Not in constant time - O(n) relative to key length.
+// .Could be more random.
