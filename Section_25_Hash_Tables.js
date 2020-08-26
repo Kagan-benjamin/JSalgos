b2652232275f9ef43a6c97f1856c34a5ea7bc90a
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-08-26 12:07:37 -0400

    Hash Table set implementation

--- Section_25_Hash_Tables.js
+++ Section_25_Hash_Tables.js
@@ -80,4 +80,39 @@ sophisticated data structure (eg array or linked list).
 the next empty slot. No need to used a nested structure. **Limited to size of hash**
 */
 
+class HashTable{
+    constructor(size=53){
+        this.keyMap = new Array(size);
+    }
 
+    _hash(key){
+        let total = 0;
+        let WEIRD_PRIME = 31;
+        for(let i = 0; i < Math.min(key.length, 100); i++){
+            let value = key[i].charCodeAt(0) - 96;
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+        return total;
+    }
+
+// .set() pseudocode:
+// 1. Accepts a key and a value
+// 2. Hashes the key 
+// 3. Stores the key-value pair in the hash table array via separate chaining.
+
+    set(key, value){
+        let index = this._hash(key);
+        if(!this.keyMap[index]){
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+    }
+
+// .get() pseudocode:
+// 1. Accepts a key
+// 2. Hashes the key
+// 3. Retrieves the key-value pair in the hash table.
+// 4. If the key isn't found, return undefined.
+
+
+}
